307c9c7e70ef2b9d99d9dcb6688a4b6b41796807
marcus <101387980+jamestorivor@users.noreply.github.com>
2026-08-21 13:42:21 +0800

    Added support for echo

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class James {
     public static void main(String[] args) {
         String greeting = "____________________________________________________________\n" +
                 "JAMES THE CHATTY CHATBOT\n" +
                 "Hello! I'm James.\n" +
                 "I can do anything for you!\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        String exitMessage = "____________________________________________________________\n" +
                 "Bye. Rest your eyes!\n" +
                 "____________________________________________________________";
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(greeting);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + input);
+            System.out.println("    ____________________________________________________________");
+        }
+        System.out.println(exitMessage);
+        scanner.close();
     }
 }
